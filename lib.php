6b90dee9bbc78bf67d5ccca4e63802f9224650b0
eWallah <info@eWallah.net>
2017-05-11 01:50:00 +0000

    $CFG->enablenotes

--- lib.php
+++ lib.php
@@ -40,10 +40,11 @@ function report_comments_extend_navigation_course($navigation, $course, $context
             $url = new moodle_url('/report/comments/index.php', ['course' => $course->id]);
             $navigation->add(get_string('comments'), $url, navigation_node::TYPE_SETTING, null, null, new pix_icon('i/report', ''));
         }
-        $url = new moodle_url('/notes/index.php', ['course' => $course->id]);
-        $str = get_string('notes', 'notes');
-        $navigation->add($str, $url, navigation_node::TYPE_SETTING, null, null, new pix_icon('i/report', ''));
-
+        if ($CFG->enablenotes) {
+            $url = new moodle_url('/notes/index.php', ['course' => $course->id]);
+            $str = get_string('notes', 'notes');
+            $navigation->add($str, $url, navigation_node::TYPE_SETTING, null, null, new pix_icon('i/report', ''));
+        }
     }
 }
 
